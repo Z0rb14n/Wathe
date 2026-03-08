809845efee8d025898750ca5605dbb8fcffe276a
Z0rb14n <babybluewoof@gmail.com>
2026-03-07 20:43:06 -0800

    hacky hotel thing

---
+++ src/main/java/dev/doctor4t/wathe/game/mapeffect/HotelMapEffect.java
@@ -0,0 +1,9 @@
+package dev.doctor4t.wathe.game.mapeffect;
+
+import net.minecraft.util.Identifier;
+
+public class HotelMapEffect extends KeyProvidingMapEffect {
+    public HotelMapEffect(Identifier identifier) {
+        super(identifier);
+    }
+}
---
+++ src/main/java/dev/doctor4t/wathe/game/mapeffect/KeyProvidingMapEffect.java
@@ -0,0 +1,74 @@
+package dev.doctor4t.wathe.game.mapeffect;
+
+import dev.doctor4t.wathe.api.MapEffect;
+import dev.doctor4t.wathe.index.WatheItems;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.LoreComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+public abstract class KeyProvidingMapEffect extends MapEffect {
+    public KeyProvidingMapEffect(Identifier identifier) {
+        super(identifier);
+    }
+    protected void provideKeys(ServerWorld serverWorld, List<ServerPlayerEntity> players, int rooms) {
+        Collections.shuffle(players);
+        int roomNumber = 0;
+        for (ServerPlayerEntity serverPlayerEntity : players) {
+            ItemStack itemStack = new ItemStack(WatheItems.KEY);
+            roomNumber = roomNumber % rooms + 1;
+            int finalRoomNumber = roomNumber;
+            itemStack.apply(DataComponentTypes.LORE, LoreComponent.DEFAULT, component -> new LoreComponent(Text.literal("Room " + finalRoomNumber).getWithStyle(Style.EMPTY.withItalic(false).withColor(0xFF8C00))));
+            serverPlayerEntity.giveItemStack(itemStack);
+
+            // give letter
+            ItemStack letter = new ItemStack(WatheItems.LETTER);
+
+            letter.set(DataComponentTypes.ITEM_NAME, Text.translatable(letter.getTranslationKey()));
+            int letterColor = 0xC5AE8B;
+            String tipString = "tip.letter.";
+            letter.apply(DataComponentTypes.LORE, LoreComponent.DEFAULT, component -> {
+                        List<Text> text = new ArrayList<>();
+                        UnaryOperator<Style> stylizer = style -> style.withItalic(false).withColor(letterColor);
+
+                        Text displayName = serverPlayerEntity.getDisplayName();
+                        String string = displayName != null ? displayName.getString() : serverPlayerEntity.getName().getString();
+                        if (string.charAt(string.length() - 1) == '\uE780') { // remove ratty supporter icon
+                            string = string.substring(0, string.length() - 1);
+                        }
+
+                        text.add(Text.translatable(tipString + "name", string).styled(style -> style.withItalic(false).withColor(0xFFFFFF)));
+                        text.add(Text.translatable(tipString + "room").styled(stylizer));
+                        text.add(Text.translatable(tipString + "tooltip1",
+                                Text.translatable(tipString + "room." + switch (finalRoomNumber) {
+                                    case 1 -> "grand_suite";
+                                    case 2, 3 -> "cabin_suite";
+                                    default -> "twin_cabin";
+                                }).getString()
+                        ).styled(stylizer));
+                        text.add(Text.translatable(tipString + "tooltip2").styled(stylizer));
+
+                        return new LoreComponent(text);
+                    }
+            );
+            serverPlayerEntity.giveItemStack(letter);
+        }
+    }
+    @Override
+    public void initializeMapEffects(ServerWorld serverWorld, List<ServerPlayerEntity> players) {
+        provideKeys(serverWorld, players, 7);
+    }
+
+    @Override
+    public void finalizeMapEffects(ServerWorld serverWorld, List<ServerPlayerEntity> players) {
+    }
+}
